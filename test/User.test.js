b64b70b3d17ebc07f832cc716f5a31b51918c3bc
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 13:30:47 -0500

    Modelado el test del segundo requerimiento y verificando primer fallo

--- test/User.test.js
+++ test/User.test.js
@@ -18,3 +18,24 @@ describe("Unit tests for User class", () => {
 
     })
 });
+
+
+/** Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento 
+ * en que se instancie un nuevo objeto de la clase `User`. */
+
+ describe("Unit tests for User class", () => {
+    test("2) Use current date in parameters", () => {
+        //Aquí va el código que se usará en la app
+        const user = new User(1,"gabrielletorricelli","Gabriel Torres Mendoza","Bio"); //Ya que la fecha actual se agregará en la definición de la clase no agregamos info en los campos
+
+        //Se validan los resultados del código.
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("gabrielletorricelli")
+        expect(user.name).toBe("Gabriel Torres Mendoza")
+        expect(user.bio).toBe("Bio")
+                    //Ya que la fecha actual se agregará en la definición de la clase solo se verificará que llegue
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+
+    })
+});
