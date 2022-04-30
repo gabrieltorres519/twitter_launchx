a9dd6dc0782ad459be0d9048375bf450a0dfc1ad
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 16:00:26 -0500

    Probando el primer fallo de la prueba para el requerimiento 4

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -56,7 +56,7 @@ describe("Unit test for UserService class",()=>{
 
 
 
-            
+
 
             // Requerimiento 3: pasa la prueba 
 
@@ -67,5 +67,19 @@ describe("Unit test for UserService class",()=>{
 
                 expect(user.username).toBe("gabo")
             })
+
+
+
+             // Requerimiento 4: pasa la prueba 
+             test("4. Given a list of users give me the list of usernames", () => {
+                const user1 = UserService.create(1, "carlogilmar1", "Carlo")
+                const user2 = UserService.create(1, "carlogilmar2", "Carlo")
+                const user3 = UserService.create(1, "carlogilmar3", "Carlo")
+                const usernames = UserService.getAllUsernames([user1, user2, user3])
+                expect(usernames).toContain("carlogilmar1")
+                expect(usernames).toContain("carlogilmar2")
+                expect(usernames).toContain("carlogilmar3")
+              })
+
 
 })
