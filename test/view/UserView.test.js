631b013f87d31565a8ee6cb9010249060e1d975c
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 19:31:38 -0500

    Segunda y tercera pruebas realizadas se han pasado

--- test/view/UserView.test.js
+++ test/view/UserView.test.js
@@ -11,4 +11,17 @@ describe("Tests for UserView", () => {
     expect(result.error).toMatch(/payload no existe/)
   })
 
+  test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+    const payload = {username: null, name: 12, id: "id"}
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  })
+
+  test("Return an error object when try to create a new user with a payload with missing properties", () => {
+    const payload = {username: "Username"}
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  })
+
+
 })
